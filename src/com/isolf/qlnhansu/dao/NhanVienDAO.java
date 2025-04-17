5ecd36dd3c88cebc963dcb281f8609f0183b65c0
TrungDoan-1304 <kannakamui1995@gamil.com>
2025-04-17 19:51:51 +0700

    thêm file sql và kết nối #1

--- src/com/isolf/qlnhansu/dao/NhanVienDAO.java
+++ src/com/isolf/qlnhansu/dao/NhanVienDAO.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package nhanvien;
+package com.isolf.qlnhansu.dao;
 
 import java.util.*;
 
